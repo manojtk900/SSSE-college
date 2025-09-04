56a054ddf25a54ed8f2bf41fc0a62dceceae84b3
Manoj TK <manojtk900869@gmail.com>
2025-09-04 13:42:36 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include("includes/header.php"); ?>
+<H1>VISVESVARAYA TECHNOLOGICAL UNIVARSITY, BELAGAVI</H1>
+<h2>SRI SIDDARTHA SCOOL OF ENGINEERING, TUMKUR </h2>
+
+<a href="https://results.vtu.ac.in/">click hear to VTU website</a>
+<form method="post" action="result.php">
+  Enter USN: <input type="text" name="usn" required>
+  <input type="submit" value="Get Result">
+</form>
+
+<br>
+<a href="admin_login.php"><button>Admin Login</button></a>
+<?php include("includes/footer.php"); ?><BR>
+<img src="mktk.jpg" width="1400" height="400"><br>
